a71736e8e3c89e7ec4d3f494c6416710f6fa1a02
David Smith <1734595+daveismith@users.noreply.github.com>
2025-09-26 16:25:52 -0400

    Move to stamped messages in the simulator.

--- launch/joystick.launch.py
+++ launch/joystick.launch.py
@@ -25,21 +25,11 @@ def generate_launch_description():
             remappings=[('/cmd_vel','/cmd_vel_joy')]
          )
 
-    twist_stamper = Node(
-            package='twist_stamper',
-            executable='twist_stamper',
-            parameters=[{'use_sim_time': use_sim_time}],
-            remappings=[('/cmd_vel_in','/diff_cont/cmd_vel_unstamped'),
-                        ('/cmd_vel_out','/diff_cont/cmd_vel')]
-         )
-
-
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
             default_value='false',
             description='Use sim time if true'),
         joy_node,
-        teleop_node,
-        twist_stamper       
+        teleop_node
     ])
